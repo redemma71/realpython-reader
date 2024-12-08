49d13ca2a413484b0ee5586dbb17bd3294526356
Chad David Cover <chad.david.cover@gmail.com>
2024-12-08 11:04:47 -0500

    First commit

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir assets
+mkdir assets/fonts
+mkdir assets/icons
+mkdir assets/images
+mkdir assets/styles
+mkdir data
+mkdir utilities
+mkdir src
+touch src/app.py
+mkdir tests
+touch tests/test_app.py
+touch LICENSE
+touch README.md
+touch MANIFEST.in
+touch pyproject.toml
+touch .gitignore
